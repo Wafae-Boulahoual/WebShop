f74547448533375b763d91e3809e908e2a1d0aaa
wafae Boulahoual <wafae.essbai@gmail.com>
2026-01-12 23:33:34 +0100

    skapat models fälte med klasser och relationer mellan klasser

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VardagshörnanApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
